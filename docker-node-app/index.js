cf7c65dd1df96630e5a0bbe0cfca4289296618f9
Mame <mazad.contractor@libertyglobal.com>
2023-01-03 15:38:50 +0100

    update docker file

--- docker-node-app/index.js
+++ docker-node-app/index.js
@@ -2,6 +2,6 @@ const express = require ('express');
 
 const app = express();
 
-app.get('/', (req, res)=>{res.send('Bye Mame ...!!!')});
+app.get('/', (req, res)=>{res.send('Byeee Mamé ....!!!')});
 
-app.listen(8050, ()=> console.log('Listening on port  8080'));
+app.listen(8080, ()=> console.log('Listening on port  8080'));
